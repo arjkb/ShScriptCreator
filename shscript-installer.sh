94aadfdbed340d290a7d30b3fa578c43dfe3ecf6
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2015-08-09 15:38:40 +0530

    Script now updating the contents of the shell's rc file

--- shscript-installer.sh
+++ shscript-installer.sh
@@ -9,3 +9,14 @@ cat scriptcode.sh >> shscript.sh
 mkdir ~/ShScriptCreator && mv shscript.sh ~/ShScriptCreator
 
 chmod +x ~/ShScriptCreator/shscript.sh   # change permission
+
+
+if [ $SHELL == "/bin/zsh" ]
+then
+	cat rc-update >> ~/.zshrc
+else
+	if [ $SHELL == "/bin/bash" ]
+	then
+		cat rc-update >> ~/.bashrc
+	fi
+fi
